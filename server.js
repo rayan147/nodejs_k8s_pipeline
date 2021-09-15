7091d4e8d1c063d438d59c9c7ccdc3312b08e803
Rayan Ramirez <rayan361@gmail.com>
2021-09-15 00:49:57 -0400

    added github

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hello World ...');
 });
+app.get('/api', (req, res) => {
+  res.send('api ...');
+});
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT} ...`);

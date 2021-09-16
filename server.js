489e6798149b72ff4062965f54bc826736f6939f
Rayan Ramirez <rayan361@gmail.com>
2021-09-16 13:12:24 -0400

    added webhooks to jenkins

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World .q..');
+  res.send('updated version');
 });
 app.get('/api', (req, res) => {
   res.send('api e...o');
